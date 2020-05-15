640d0acf7a9833526d29976eb685980f5b04b1e1
Ashutosh <ashutoshcpr@gmail.com>
2020-05-15 12:32:37 +0530

    cleaned import

--- src/main/java/com/xyz/apiproxy/ApiGatewayProxyApplication.java
+++ src/main/java/com/xyz/apiproxy/ApiGatewayProxyApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
